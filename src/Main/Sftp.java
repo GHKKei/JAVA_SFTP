bee220022cfa8aad22e41b5e869d92e290f758e7
kevin <kevinkei1803@gmail.com>
2022-08-09 14:34:01 +0700

    SFTP

--- src/Main/Sftp.java
+++ src/Main/Sftp.java
@@ -27,7 +27,7 @@ public class Sftp {
     public static void main(String[] arg) {
 
         try {
-            System.out.println("OK");
+//            System.out.println("OK");
             JSch jsch = new JSch();
 
             String host = null;
